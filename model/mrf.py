1f902ae3e28a741b76508311aa381213953dbd5b
cuongnguyengit <cuongnguyen170898@gmail.com>
2020-12-29 05:27:55 +0700

    .

---
+++ model/mrf.py
@@ -0,0 +1,16 @@
+from model.resstack import ResStack
+from torch import nn
+from utils.utils import weights_init
+
+class MRF(nn.Module):
+    def __init__(self, kernels, channel, dilations = [[1,1], [3,1], [5,1]]):
+        super(MRF, self).__init__()
+        self.resblock1 = ResStack(kernels[0], channel, 0)
+        self.resblock2 = ResStack(kernels[1], channel, 6)
+        self.resblock3 = ResStack(kernels[2], channel, 12)
+
+    def forward(self, x):
+      x1 = self.resblock1(x)
+      x2 = self.resblock2(x)
+      x3 = self.resblock3(x)
+      return x1 + x2 + x3
